3166aa2c01982e29a9f15d24d918f640244755c1
fnsl223 <fnsl223@naver.com>
2025-07-27 17:32:59 +0900

    Initial commit: Add responsive portfolio website

---
+++ custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  // Trigger
+  console.log("jQuery 로드됨!"); // 이것도 추가해서 테스트
+  $(".trigger").click(function () {
+    console.log("클릭됨!");
+    $(this).toggleClass("active");
+    $(".gnb").toggleClass("active");
+  });
+  $("section, .menu a").click(function () {
+    $(".gnb").removeClass("active");
+    $(".trigger").removeClass("active");
+  });
+
+  //   Smooth Scrolling
+  $(".menu a, .gototop").click(function (e) {
+    $.scrollTo(this.hash || 0, 900);
+  });
+
+  //  Change CSS with Scroll
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+      $("header, .gototop").addClass("active");
+    } else {
+      $(window).scrollTop() < 50;
+      $("header, .gototop").removeClass("active");
+    }
+  });
+});
