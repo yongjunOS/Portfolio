c48b175877db7529e18d20e0d1772a3bd16a111c
fnsl223 <fnsl223@naver.com>
2025-08-03 20:04:27 +0900

    moblie responsive  add

--- custom.js
+++ custom.js
@@ -33,6 +33,34 @@ $(document).ready(function () {
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
   });
 
   //Slick.js :  Project photo
@@ -43,6 +71,6 @@ $(document).ready(function () {
     fade: true,
     autoplay: true,
     autoplayspeed: 2000,
-    cssEase: "linear"
+    cssEase: "linear",
   });
 });
